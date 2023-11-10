17a27eb42216011a1e95c42c3e9a3ea788efc813
leandrodeleon22 <leandrodeleon22@gmail.com>
2023-11-09 22:25:08 -0500

    beeping at the specific time

---
+++ timer1.js
@@ -0,0 +1,43 @@
+const timeToBeep = process.argv.slice(2);
+console.log(timeToBeep);
+//[ '10', '3', '5', '15', '9' ]
+//get the highest number
+//loop the given secs
+//setTimeout inside the loop
+// set time
+const numberToBeep = timeToBeep.map((el) => {
+  return Number(el);
+});
+
+const highestNumber = numberToBeep.reduce((acc, current) => {
+  if (acc > current) return acc;
+  return current;
+}, numberToBeep[0]);
+
+let time = 1;
+let timeOut = 1000;
+
+for (let index = 1; index <= highestNumber; index++) {
+  setTimeout(() => {
+    console.log(time);
+    if (numberToBeep.includes(time)) {
+      process.stdout.write("\x07");
+    }
+    time += 1;
+  }, timeOut);
+
+  timeOut += 1000;
+}
+
+// timeToBeep.forEach((element) => {
+//   setTimeout(() => {
+//     console.log(time);
+//     time += 1;
+//   }, timeOut);
+
+//   timeOut += 1000;
+// });
+
+// console.log(highestNumber);
+
+// process.stdout.write("\x07");
